216d17ccc07649b6b18a12616cd1f11177ec00f7
hihowareyou2 <sjmmole@live.co.uk>
2021-07-18 15:14:53 +0100

    Basic Todo list with Add/Delete/Edit Functionality

---
+++ editbutton.js
@@ -0,0 +1,85 @@
+
+// Variables used by script.
+// We need references to the textarea, the button and the div to place the results in
+var userInput = document.getElementById('user-input'),
+    saveButton = document.getElementById('btn'),
+    resultEl = document.getElementById('saved-text');
+
+// Helper function to help keep code clean...
+// All it does is make a button element, which we then add to each of the p 
+// elements we add to the page
+function makeMasterDeleteButton() {
+    var btn = document.createElement('a');
+    btn.textContent = 'X';
+    btn.classList.add('item-delete'); // give button the class "item-delete"
+    btn.setAttribute('href', '#');
+    return btn;
+}
+
+function makeMasterEditButton() {
+    var btn = document.createElement('a');
+    btn.textContent = 'Edit';
+    btn.classList.add('item-edit'); // give button the class "item-delete"
+    btn.setAttribute('href', '#');
+    return btn;
+}
+
+// Make sure we have valid elements to work with
+if (userInput !== null && saveButton !== null && resultEl !== null) {
+
+    // Listen for clicks on form button
+    saveButton.addEventListener('click', function (event) {
+
+        var newNode, // Element we will create
+            itemEditButton,
+            itemDeleteButton, // Delete button for element
+            submitted = userInput.value; // Text from form field
+
+        // Check if user entered something
+        if (submitted !== '') {
+
+            // Make a p element and add the submitted text to it
+            newNode = document.createElement('p');
+            newNode.textContent = submitted;
+
+            // Make a edit button element and append it to p element
+            itemEditButton = makeMasterEditButton();
+            newNode.insertAdjacentElement('afterbegin', itemEditButton);
+
+            // Make a delete button element and append it to p element
+            itemDeleteButton = makeMasterDeleteButton();
+            newNode.insertAdjacentElement('afterbegin', itemDeleteButton);
+
+            // add the p element to the page
+            resultEl.appendChild(newNode);
+
+            // Erase user's text from the textarea
+            userInput.value = '';
+        }
+
+        // Stop the browser's default behaviour. By default, when a "button" within a "form"
+        // element is clicked, the form gets submitted, resulting in a page reload (which we don't want)
+        event.preventDefault();
+    });
+
+    // Add event listener for item deletions here...
+
+    resultEl.addEventListener('click', function(event) {
+        event.preventDefault();
+        var attr = event.target.classList.contains('item-delete');
+        if(attr) {
+          resultEl.removeChild(event.target.parentNode);
+        }
+    });
+
+    resultEl.addEventListener('click', function(event) {
+        event.preventDefault();
+        var attr = event.target.classList.contains('item-edit');
+        var editText = resultEl.getElementsByTagName('p')[0].childNodes[2].nodeValue;
+        if(attr) {
+            resultEl.removeChild(event.target.parentNode);
+            userInput.value = editText;
+        }
+    });
+}
+
